8df5fea3924e1b836af5e563618c052183854201
kmnyn <mengyan717@gmail.com>
2024-07-08 21:26:09 +0800

    changed BASE_URL

--- Login_Register/Frontend/static/js/config.js
+++ Login_Register/Frontend/static/js/config.js
@@ -1,2 +1,2 @@
 const isDeploy = true;
-const BASE_URL = isDeploy ? "https://monojar-test.onrender.com" : "http://127.0.0.1:5000";
+const BASE_URL = isDeploy ? "https://milestone3-fut9.onrender.com" : "http://127.0.0.1:5000";
